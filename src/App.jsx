178264e895a4b28e6c26e65de8d6cd72dfb38c44
Raghibjamil <raghibj123@gmail.com>
2024-05-06 14:25:40 +0530

    completed up to Started adding basic redux setup with toolkit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { Routes } from 'react-router-dom';
+
+function App() {
+
+
+  return (
+    <>
+    
+    <h1 className='text-2xl text-red-900 underline'>At this situation we can not able to use router directly...</h1>
+  
+
+
+    {/* 
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+      </Routes> 
+    */}
+
+    </>
+  )
+}
+
+export default App
