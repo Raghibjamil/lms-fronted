cecb27c4e24d62c6bb3bfa3db6248642213810fa
Raghibjamil <raghibj123@gmail.com>
2024-05-09 21:40:25 +0530

    About us pages created successfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes ,Route} from 'react-router-dom';
 import Footer from './Components/Footer';
 import HomePage from "./Pages/HomePage";
+import AboutUs from './Pages/AboutUs';
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
     
     <Routes>
         <Route path="/" element={<HomePage />} ></Route>
+        <Route path="/about" element={<AboutUs />} ></Route>
+
 
       </Routes>
     </>
