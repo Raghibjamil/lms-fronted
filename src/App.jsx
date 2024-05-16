88603c01032677588f8ee7c3368bd1be27c3ed12
Raghibjamil <raghibj123@gmail.com>
2024-05-16 12:09:34 +0530

    Prepared course details component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from './Pages/Login';
 import CourseList from './Pages/Course/CourseList';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
+import CourseDescription from './Pages/Course/CourseDescription';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="/denied" element={<Denied />} />
 
+        <Route path="/course/description" element={<CourseDescription />} />
 
 
         <Route path="/signup" element={<Signup />} />
